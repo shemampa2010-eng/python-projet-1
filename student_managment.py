c41c114afc9b41aa48f80e284c6343378f92cb5d
Paulin <shemampa2010@gmail.com>
2026-06-10 15:38:56 +0200

    Calcule la moyenne des notes obtenu par les eleves

---
+++ student_managment.py
@@ -0,0 +1,10 @@
+students_marks = [78,85,92,45,67]
+#for student_mark in range(len(students_marks)):
+ #numero_etudiant = student_mark + 1
+ #note_etudiant = students_marks [student_mark]
+ #print ("Studiant" , numero_etudiant ,":", note_etudiant)
+ 
+total = sum(students_marks)
+average = total/len(students_marks)
+print(average)
+ 
