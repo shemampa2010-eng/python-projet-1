cc3d048fd4b0e6323b0dbe66ab99dc27aee78b8c
Paulin <shemampa2010@gmail.com>
2026-06-10 16:38:04 +0200

    ce programme donne une note minimal pour etre admis ou refuse, indiquant meme le resultat.

--- student_managment.py
+++ student_managment.py
@@ -1,10 +1,19 @@
+needed_to_pass = 50
 students_marks = [78,85,92,45,67]
-#for student_mark in range(len(students_marks)):
- #numero_etudiant = student_mark + 1
- #note_etudiant = students_marks [student_mark]
- #print ("Studiant" , numero_etudiant ,":", note_etudiant)
- 
-total = sum(students_marks)
-average = total/len(students_marks)
-print(average)
+for student_mark in range(len(students_marks)):
+ numero_etudiant = student_mark + 1
+ note_etudiant = students_marks [student_mark]
+
+
+# total = sum(students_marks)
+# average = total/len(students_marks)
+index = 1
+for student_mark in students_marks :
+
+ if student_mark < needed_to_pass :
+  print ("Studiant" , index , ":", student_mark , "Refuse")
+ else :
+  print("Studiant" , index ,":", student_mark, "Admis" )
+ index += 1
+
  
