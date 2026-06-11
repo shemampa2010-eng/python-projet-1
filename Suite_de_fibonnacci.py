9db6cd12e152f99dffb8093b814d25fadc08cb50
Paulin <shemampa2010@gmail.com>
2026-06-11 15:42:15 +0200

    Ce programme vous trouve le factorielle de votre nombre

--- Suite_de_fibonnacci.py
+++ Suite_de_fibonnacci.py
@@ -1,13 +1,14 @@
-nombre = int(input("Jusqu'a quel rang voulez-vous aller ? "))
-a = 0
-b = 1
-print(a,"\n",b)
+def fibonnacci():
+    nombre = int(input("Jusqu'a quel rang voulez-vous aller ? "))
+    a = 0
+    b = 1
+    print(a,"\n",b)
 
-for i in range(1, nombre):
-    print ("current:", a, "current:",b)
-    c = a+b
-    print(c)
+    for i in range(1, nombre):
+        print ("current:", a, "current:",b)
+        c = a+b
+        print(c)
 
-    a= b
-  
-    b= c
+        a= b
+    
+        b= c
--- factorielle.py
+++ factorielle.py
@@ -1,5 +1,13 @@
+def factorielle(nombre) :
+    factorial = 1
+    for i in range (nombre, 1, -1):
+        factorial = factorial *i
+    if n < 1 :
+        print ("Sorry, the program needs a number over zero")
+    else :
+        print (factorial)    
+
 n= int(input("Wanna find the factorial of which number?"))
-factorial = 1
-for i in range (n, 1, -1):
-    factorial = factorial *i
-print (factorial)    
+factorielle(n )
+
+
