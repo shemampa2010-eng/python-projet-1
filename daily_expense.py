50c52011cf6ff053e51683022d8d924a6e215760
Paulin <shemampa2010@gmail.com>
2026-06-12 12:12:49 +0200

    A program that helps a user track their daily expenses.

--- daily_expense.py
+++ daily_expense.py
@@ -1,5 +1,27 @@
+def higher(my_expenses):
+    superieure = my_expenses[0]
+    for el in my_expenses :
+        if el > superieure :
+            superieure = el
+    return superieure        
+
+def my_sumation (elements):
+    mysum = 0
+    for element in elements :
+        mysum += element
+    return mysum    
+    
 total_exp = int(input ("How many times have you spent money today?"))
+
 list_exp = []
+
 for i in range(1,total_exp +1):
     expense = int(input(f"expense {i} :"))
     list_exp.append (expense)
+  
+total = my_sumation(list_exp)
+average = total/total_exp
+
+print( "Expense Summary", "\n", "Number of expenses :", total_exp, "\n", 
+"Total money spent :", total, "\n", "Average money spent today :", average, "\n", 
+"Highest expense :" , higher(list_exp))
